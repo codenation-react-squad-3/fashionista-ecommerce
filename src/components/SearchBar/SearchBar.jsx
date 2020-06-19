37697c82df54ec0ac699dff0f4135e2d017f24a9
maira <marry.alves@hotmail.com>
2020-06-19 19:45:21 -0300

    feat/base search bar structure and debounce function

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react'
+import {useState} from 'react'
+import useDebounce from '../../utils/useDebounce';
+import './SearchBar.css'
+
+const SearchBar = (props) => {
+    const [searchQuery, setSearchQuery] = useState('')
+    const [searchProductResult, setSearchProductResult] = useState([])
+    const debouncedTerm = useDebounce(searchQuery, 500)
+    
+    // PRODUCTLIST FICTICIO
+    const productList = [{name: 'camila'}, {name: 'joana'}, {name: 'maira'}, {name: 'maiara'}]
+    let results
+
+    useEffect(()=> {
+        function search() {
+            if(debouncedTerm){
+                const searchResult = productList.filter(product => product.name.includes(debouncedTerm))
+                setSearchProductResult(searchResult)
+            } else {setSearchProductResult([])}
+        }
+        search();
+    }, [debouncedTerm])
+
+    if(searchProductResult.length){
+        results = searchProductResult.map(item => {
+            return <span key={item.name}>{item.name}</span>
+        })
+    } else { results = (<span className="search__result search__result--empty">Nenhum item encontrado :\</span>)}
+
+    return (
+        <div>
+            <div className="search__area">
+                <input type="text" className="search__input" placeholder="Buscar por produto..." onChange={text => setSearchQuery(text.target.value)}></input>
+            </div>
+            <div>
+                {results}
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
