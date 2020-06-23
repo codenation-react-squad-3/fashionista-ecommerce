51f019e60917cba8ed1016e5d133409b66d72718
maiaraalves <marry.alves@hotmail.com>
2020-06-23 19:45:55 -0300

    added modal and modal opens when click on search icon

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
-
+//import ReactDOM from 'react-dom';
 import { FiArrowLeft} from 'react-icons/fi'
-
 import './Modal.scss'
+import SearchBar from '../SearchBar/SearchBar'
 
-const Modal = () => {
-    return (
-        <div className="modal">
-            <div className="modal__container">
-                <div className="modal__header">
-                    <button className="modal__btn">
-                        <FiArrowLeft/>
-                    </button>
-                    <div className="modal__title">
-                        <h1>Title goes here</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+const Modal = ({ isShowing, hide }, isSearchModal) => {
+	const searchModal = (
+		<div>
+			<SearchBar/>
+		</div>
+	)
+	
+	if (isShowing && isSearchModal) {
+		return (
+			<div className="modal__overlay">
+			<div className="modal__container">
+				<div className="modal">
+					<div className="modal__header">
+						<button className="modal__btn"  aria-label="Close" onClick={hide}>
+							<FiArrowLeft/>
+						</button>
+						<div className="modal__title">
+								<h1>Title goes here</h1>
+						</div>
+					</div>
+					{searchModal}
+				</div>
+			</div>
+		</div>
+		)
+	} else {
+		return <div></div>
+	}
 }
 
 export default Modal;
